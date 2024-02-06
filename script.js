9b1fa7735521ad3556f7b207cc44aa1fedb63f1f
imnotfez <kevinmleeuwerke@outlook.com>
2024-02-06 07:56:28 +0100

    final

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let cursedAmount = 25000000000000000000000000000000;
+let cursedAmount = 12312783;
+let ultraCursedAmount
 let clickingPower = 1;
 let upgradeOneCost = 15;
 let upgradeOne = 0;
@@ -9,6 +10,12 @@ let upgradeThree = 0;
 let upgradeFourCost = 10000;
 let upgradeFour = 0;
 let cursedEnergyPerSecond = 0;
+let ultracursedEnergyPerSecond
+let fingersEaten = 0;
+let fingersOwned = 0;
+let fingerPower = 0;
+let fingerEatCost = 100000;
+let ghostcatOwned = 0;
 
 
 
@@ -16,7 +23,7 @@ function buyUpgradeOne() {
     if (cursedAmount >= upgradeOneCost) {
         cursedAmount = cursedAmount - upgradeOneCost;
         upgradeOne = upgradeOne + 1;
-        upgradeOneCost = Math.round(upgradeOneCost * 1.05);
+        upgradeOneCost = Math.round(upgradeOneCost * 1.1);
         document.getElementById("cursedAmount").innerHTML = cursedAmount;
         document.getElementById("upgradeOneCost").innerHTML = upgradeOneCost;
         document.getElementById("upgradeOne").innerHTML = upgradeOne;
@@ -28,7 +35,7 @@ function buyUpgradeTwo() {
     if (cursedAmount >= upgradeTwoCost) {
         cursedAmount = cursedAmount - upgradeTwoCost;
         upgradeTwo = upgradeTwo + 1;
-        upgradeTwoCost = Math.round(upgradeTwoCost * 1.1);
+        upgradeTwoCost = Math.round(upgradeTwoCost * 1.15);
         document.getElementById("cursedAmount").innerHTML = cursedAmount;
         document.getElementById("upgradeTwoCost").innerHTML = upgradeTwoCost;
         document.getElementById("upgradeTwo").innerHTML = upgradeTwo;
@@ -48,8 +55,40 @@ function buyUpgradeThree() {
     }
 }
 
+function buyUpgradeFour() {
+    if ( cursedAmount >= upgradeFourCost ) {
+        cursedAmount = cursedAmount - upgradeFourCost;
+        upgradeFour = upgradeFour + 1;
+        upgradeFourCost = Math.round(upgradeFourCost * 1.35);
+        document.getElementById("cursedAmount").innerHTML = cursedAmount
+        document.getElementById("upgradeFourCost").innerHTML = upgradeFourCost;
+        document.getElementById("upgradeFour").innerHTML = upgradeFour;
+        updateEnergyPerSecond();
+    }
+}
 
-// Add to score
+function buyFingerUpgrade() {
+    if (cursedAmount >= fingerEatCost) {
+        cursedAmount = cursedAmount - fingerEatCost
+        fingersEaten = fingersEaten + 1;
+        fingerPower + 1000;
+        updateEnergyPerSecond();
+        
+    }
+}
+
+function BuyFirstPet() {
+    if ( fingersEaten >= 1)
+    {
+        fingersEaten = fingersEaten - 1;
+        fingersOwned = fingersOwned + 1;
+        cursedEnergyPerSecond = cursedEnergyPerSecond - 100000;
+        ghostcatOwned = ghostcatOwned + 1
+        document.getElementById("ghostcatOwned").innerHTML = ghostcatOwned;
+        updateEnergyPerSecond();
+    }
+}
+
 function addToCursedEnergy(amount) {
     cursedAmount = cursedAmount + amount;
     document.getElementById("cursedAmount").innerHTML = cursedAmount;
@@ -58,11 +97,18 @@ function addToCursedEnergy(amount) {
 function updateEnergyPerSecond() {
     cursedEnergyPerSecond = upgradeOne + upgradeTwo  * 10;
     cursedEnergyPerSecond = cursedEnergyPerSecond + upgradeThree * 100;
+    cursedEnergyPerSecond = cursedEnergyPerSecond + upgradeFour * 250;
+    cursedEnergyPerSecond = cursedEnergyPerSecond + fingersEaten * 0;
+    cursedEnergyPerSecond = cursedEnergyPerSecond + ghostcatOwned * 1000;
     document.getElementById("energyPerSecond").innerHTML = cursedEnergyPerSecond
 }
 
 setInterval(function() {
     cursedAmount = cursedAmount + upgradeOne;
     cursedAmount = cursedAmount + upgradeTwo + upgradeThree * 10;
+    cursedAmount = cursedAmount + upgradeFour * 250;
+    cursedAmount = cursedAmount + ghostcatOwned * 1000;
     document.getElementById("cursedAmount").innerHTML = cursedAmount;
-}, 1);
+    document.getElementById("fingersEaten").innerHTML = fingersEaten
+    document.getElementById("fingersOwned")
+}, 1000);
